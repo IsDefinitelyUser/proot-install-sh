79be5c1f147c8ed2f295269c5dabb828e52abb38
Dana Delilah Burns <xmwx38@disroot.org>
2022-02-07 16:13:44 +0000

    [Description]: Branch 'main' created; Added .sh files.

---
+++ alpine-xfce-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Alpine + XFCE Desktop Manager
+echo "Install – Alpine + XFCE Desktop Manager"
+curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/repo-fix.sh > repo.sh && chmod +x repo.sh && bash repo.sh && pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Alpine/alpinexfce.sh -O alpinexfce.sh && chmod +x alpinexfce.sh && bash alpinexfce.sh
